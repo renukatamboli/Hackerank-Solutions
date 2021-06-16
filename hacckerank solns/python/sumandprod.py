33d8636312c4c4ac6e875842dbd17d3a11eaf1cc
renukatamboli <tambolirenuka2@gmail.com>
2021-06-16 22:18:36 +0530

    Create sumandprod.py

---
+++ hacckerank solns/python/sumandprod.py
@@ -0,0 +1,10 @@
+import numpy
+n,m = map(int,raw_input().split())
+lst= []
+for i in range(0,n):
+    lst.append(map(int,raw_input().split()))
+my_array = numpy.array(lst)
+print(numpy.prod(numpy.sum(my_array, axis = 0))) 
+
+
+
