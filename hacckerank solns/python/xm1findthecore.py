c7b609ae97fa9ff7e09565ce734b6e3831272e45
renukatamboli <tambolirenuka2@gmail.com>
2021-06-30 22:57:26 +0530

    Create xm1findthecore.py

---
+++ hacckerank solns/python/xm1findthecore.py
@@ -0,0 +1,16 @@
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(node):
+    count = len(node.attrib)
+    for child in node:
+        count = count+ get_attr_number(child)          
+    return count    
+    # your code goes here
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
