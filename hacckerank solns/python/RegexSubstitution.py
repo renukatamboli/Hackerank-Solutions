0623d44f11a0852b4fef1f87ca36208bfff478c4
renukatamboli <tambolirenuka2@gmail.com>
2021-07-17 13:02:47 +0530

    Create RegexSubstitution.py

---
+++ hacckerank solns/python/RegexSubstitution.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+n = int(input())
+for i in range(0,n):
+    print(re.sub(r"\s\|\|(?=\s)", " or",re.sub(r"\s&&(?=\s)"," and",input())))
