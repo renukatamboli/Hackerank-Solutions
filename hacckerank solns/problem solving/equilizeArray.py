ed696b989354c82365527ecc471b8bbcd81d7bed
renukatamboli <tambolirenuka2@gmail.com>
2021-09-25 22:40:33 +0530

    Create equilizeArray.py

---
+++ hacckerank solns/problem solving/equilizeArray.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+from collections import Counter
+def equalizeArray(arr):
+    counter = Counter(arr)
+    return len(arr) - counter.most_common(1)[0][1]
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
